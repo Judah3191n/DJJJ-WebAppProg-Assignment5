01fa8c342f7b621633f4c7bc23042fd825dbab31
Judah319n <w0509673@nscc.ca>
2025-10-06 11:36:23 -0300

    Never gonna run around and desert you

--- Assignment5DJJJ/src/main/java/com/Assignment5DJJJ/Assignment5DJJJ/WelcomeController.java
+++ Assignment5DJJJ/src/main/java/com/Assignment5DJJJ/Assignment5DJJJ/WelcomeController.java
@@ -1,19 +1,45 @@
 package com.Assignment5DJJJ.Assignment5DJJJ;
+
 import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class WelcomeController {
-   
-      @PostMapping("/login")
+  public ArrayList<User> users = new ArrayList<User>();
+
+  @PostMapping("/login")
     public String processLogin(@RequestParam String username, @RequestParam String password, Model model) {
-}
 
   @GetMapping("/login")
-    public String showLoginForm() {
-        return "login";
+  public String showLoginForm() {
+    return "login";
+  }
+
+  // Register
+  @GetMapping("/register")
+  public String showRegisterForm() {
+    return "register";
+  }
+
+  @PostMapping("/register")
+  public String postMethodName(@RequestParam String username, @RequestParam String password, @RequestParam String email,
+    @RequestParam String address, Model model) {
+    for (User user : users) {
+      if (user.getName().equals(username)) {
+        model.addAttribute("usernameError", "Username already taken");
+        return "register";
+      }
     }
+    users.add(new User(username, password, email, address));
+    return "login";
+  }
+
+}
 }
