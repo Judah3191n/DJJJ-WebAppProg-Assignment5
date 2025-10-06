cca885d7a1797cf773b7baa0e134f6efb8b8e97c
Josh Leslie <joshleslie1993@gmail.com>
2025-10-06 11:26:22 -0300

    tweaked logn info for controller

--- Assignment5DJJJ/src/main/java/com/Assignment5DJJJ/Assignment5DJJJ/WelcomeController.java
+++ Assignment5DJJJ/src/main/java/com/Assignment5DJJJ/Assignment5DJJJ/WelcomeController.java
@@ -1,6 +1,6 @@
 package com.Assignment5DJJJ.Assignment5DJJJ;
-import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,12 +8,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WelcomeController {
    
-      @PostMapping("/login")
-    public String processLogin(@RequestParam String username, @RequestParam String password, Model model) {
-}
-
-  @GetMapping("/login")
+@GetMapping("/login")
     public String showLoginForm() {
         return "login";
     }
+
+      @PostMapping("/login")
+    public String processLogin(@RequestParam String username, @RequestParam String password, Model model) {
+      model.addAttribute("username", username);
+      model.addAttribute("password", password);
+
+        if ("user".equals(username) && "pass".equals(password)) {
+            model.addAttribute("message", "Login successful!");
+            return "welcome";
+        } else {
+            model.addAttribute("message", "Invalid username or password.");
+            return "login";
+        }
+    }
+
+  
 }
