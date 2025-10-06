bbfcacf031480bcd74ccc24998c7a474196aad0b
Josh Leslie <joshleslie1993@gmail.com>
2025-10-06 11:54:06 -0300

    logoutbuttion fix

--- Assignment5DJJJ/src/main/java/com/Assignment5DJJJ/Assignment5DJJJ/WelcomeController.java
+++ Assignment5DJJJ/src/main/java/com/Assignment5DJJJ/Assignment5DJJJ/WelcomeController.java
@@ -9,6 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
@@ -55,4 +58,11 @@ public class WelcomeController {
      */
     return "welcome";
   }
+
+  @GetMapping("/logout")
+  public String logout(HttpSession session) {
+    session.invalidate(); // This clears all session attributes
+    return "redirect:/login"; // Redirect to login page
+  }
 }
+
